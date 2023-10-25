51ba7241293c0a36cf6ab7ce84ebbc5876650b9b
hierach <1060829100@qq.com>
2023-10-25 16:24:55 +0800

    泛型知识第二次提交

--- src/main/java/com/weifengqin/Main.java
+++ src/main/java/com/weifengqin/Main.java
@@ -15,11 +15,11 @@ import java.util.List;
 public class Main {
 
 
-    /**
-     * 泛型两大作用
-     * ①编译时做类型擦除，做类型检查。对类型进行了编译限制。避免了强转，把类型检查提前到了编译时
-     * ②
-     */
+    //泛型优势
+//    类型安全性：泛型允许在编译时捕获类型错误，而不是在运行时。这有助于减少由于类型不匹配而引发的异常。
+//    代码复用：泛型代码可以适用于多种数据类型，减少了重复编写相似代码的工作。
+//    可读性和可维护性：泛型使代码更加清晰，因为它们表明了代码的意图，而且代码更易于维护。
+//    集合框架的增强：Java的集合框架（如ArrayList和HashMap）使用泛型来提高类型安全性和可读性。
 
 
     /**
@@ -65,6 +65,7 @@ public class Main {
         List<Integer> integerList = new ArrayList<>();
         List<String> stringList = new ArrayList<>();
         //TODO 怎么理解 这里的泛型是什么
+        //这里的泛型不带"菱形操作符" (<>，使用的时候具体推断
         List list = new ArrayList();
 
 //        PrintAnyThing.restrictedTypeWildcard(integerList);失败
@@ -75,6 +76,10 @@ public class Main {
 //        PrintAnyThing.restrictedTypeWildcard(new ArrayList<Integer>()); 失败
 //        PrintAnyThing.restrictedTypeWildcard(new ArrayList<Object>()); 失败
         //TODO 怎么理解 这里的泛型是什么
+        /**
+         * 在Java 8及更高版本中可以成功编译，因为编译器可以推断出new ArrayList<>()应该是一个ArrayList<? extends String>，
+         * 而且ArrayList<? extends String>可以匹配方法的参数类型List<? extends String>。
+         */
         PrintAnyThing.restrictedTypeWildcard(new ArrayList<>());
 
     }
